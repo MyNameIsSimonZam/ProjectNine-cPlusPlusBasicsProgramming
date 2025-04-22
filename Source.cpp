37b1cf653794f78fa7125461fe85c1e06b09e9d0
Simon <semazamesin@gmail.com>
2025-04-22 17:23:23 +0500

    task for lesson 10 was complete

--- Source.cpp
+++ Source.cpp
@@ -114,6 +114,35 @@ void task21() {	 // эталонное решение
 	else {
 		std::cout << "No\n";
 	}
+}
 
 
+void task3() {
+	float a, b, c;
+	std::cout << "a, b, c:";
+	std::cin >> a >> b >> c;
+	if (a == 0)				
+	{
+		std::cout << "Not a quadratic equation!" << std::endl;
+	}
+	else 
+	{
+		float discriminant = b * b - 4 * a * c;
 
+		if (discriminant > 0)
+		{
+			float x1 = (-b + std::sqrt(discriminant)) / (2 * a);
+			float x2 = (-b - std::sqrt(discriminant)) / (2 * a);
+			std::cout << "Root 1, 2: " << x1 << ", " << x2 << std::endl;
+		}
+		else if (discriminant == 0)
+		{
+			float x = -b / (2 * a);
+			std::cout << "Root: " << x << std::endl;
+		}
+		else
+		{
+			std::cout << "Complex scenario is not supported!" << std::endl;
+		}
+	}
+}
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 void task1();
 void task2();
+void task3();
 
 int main() {
 	int choice;
@@ -10,6 +11,7 @@ int main() {
 		std::cout << "Choose a task to run\n";
 		std::cout << "1. Задача 1\n";
 		std::cout << "2. Задача 2\n";
+		std::cout << "3. УРОК 10\n";
 		std::cout << "Enter your choice: ";
 		std::cin >> choice;
 
@@ -19,6 +21,9 @@ int main() {
 			break;
 		case 2:
 			task2();
+			break;
+		case 3:
+			task3();
 			break;
 		default:
 			std::cout << "Invalid choice...\n";
